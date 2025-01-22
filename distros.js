1e4cac663902e12fad00b891d2d04a650021e6a1
id_minaev <id_minaev@controlsystems.ru>
2025-01-22 18:49:17 +0400

    init

---
+++ distros.js
@@ -0,0 +1,104 @@
+export const distros = [
+    {
+        name: "Ubuntu",
+        description: "Ubuntu is a popular Linux distribution based on Debian. It used to be really nice, but then they added snaps and a lot of bloat. Still a solid choice",
+        color: "#FF0000", // Red
+        isBold: false,
+    },
+    {
+        name: "Fedora",
+        description: "Fedora is a cutting-edge Linux distribution sponsored by Red Hat. Solid choice, even Linus Torvalds uses it!",
+        color: "#0000FF", // Blue
+        isBold: true,
+    },
+    {
+        name: "Debian",
+        description: "Debian is a stable and versatile Linux distribution. Pretty much the holy grail of modern distros.",
+        color: "#FF0000", // Red
+        isBold: false,
+    },
+    {
+        name: "Arch Linux",
+        description: "Arch Linux is a lightweight and flexible Linux distribution. Go ahead, say the words, you use Arch btw..",
+        color: "#00FFFF", // Cyan
+        isBold: true,
+    },
+    {
+        name: "openSUSE",
+        description: "openSUSE is a versatile and stable Linux distribution. And I like the little chameleon",
+        color: "#00FF00", // BrightGreen
+        isBold: true,
+    },
+    {
+        name: "Linux Mint",
+        description: "Linux Mint is a user-friendly Linux distribution based on Ubuntu. Really a solid choice if you want to get work done. Used this one a long time myself! :D",
+        color: "#008000", // Green
+        isBold: false,
+    },
+    {
+        name: "Gentoo",
+        description: "Gentoo is a flexible and source-based Linux distribution. Have fun compiling stuff for hours haha!",
+        color: "#FF00FF", // Magenta
+        isBold: true,
+    },
+    {
+        name: "Void Linux",
+        description: "Void Linux is an independent Linux distribution focused on simplicity. Extra points because this one sounds really cool",
+        color: "#008000", // Green
+        isBold: true,
+    },
+    {
+        name: "EndeavourOS",
+        description: "EndeavourOS is Arch but without all the pain. Really nice choice! :)",
+        color: "#FF00FF", // Magenta
+        isBold: true,
+    },
+    {
+        name: "NixOS",
+        description: "NixOS is a Linux distribution based on Nix package manager. It's really cool if you like to manage your system with code and stuff",
+        color: "#FF77FF", // BrightMagenta
+        isBold: true,
+    },
+    {
+        name: "Linux From Scratch",
+        description: "Linux From Scratch huh? So you have chosen death. Just kidding, this one is really cool if you want to learn how Linux works from the ground up",
+        color: "#FF4444", // BrightRed
+        isBold: false,
+    },
+    {
+        name: "OpenBSD",
+        description: "OpenBSD is a Unix-like operating system focused on security, simplicity, and code correctness. It's a top choice for secure servers and firewalls.",
+        color: "#FFFF00", // Yellow
+        isBold: true,
+    },
+    {
+        name: "FreeBSD",
+        description: "FreeBSD is a Unix-like operating system known for its performance and advanced networking capabilities. It's great for servers, desktops, and embedded systems.",
+        color: "#FFFFFF", // White
+        isBold: false,
+    },
+    {
+        name: "Kali Linux",
+        description: "The OS for Hackers and Script Kiddies. Based on Debian and equipped with a wide range of Hacking and pentesting Tools, it is the perfect choice for Hackers and everybody who wants to become one.",
+        color: "#007FFF", // BrightBlue
+        isBold: true,
+    },
+    {
+        name: "Qubes OS",
+        description: "A Secure OS running everything in virtual Machines to protect against hacking attacks and everything else.",
+        color: "#F5F5F5", // BrightWhite
+        isBold: false,
+    },
+    {
+        name: "Tails OS",
+        description: "Put on your tinfoil hat. Tails OS is an anonymous, live OS with built in integration of Tor and I2P for the maximum of privacy",
+        color: "#696969", // BrightBlack (Gray)
+        isBold: true,
+    },
+    {
+        name: "Vanilla OS",
+        description: "Vanilla OS, allows you to install packages from most other Linux distros while staying immutable.",
+        color: "#FFFF99", // BrightYellow
+        isBold: false,
+    },
+];
---
+++ index.js
@@ -0,0 +1,127 @@
+import { distros } from "./distros.js";
+
+const resolution = {
+    heigth: 60,
+    width: 230
+}
+
+const spaceChar = '&nbsp';
+
+// Вероятность появления звезды в процентах
+const starProbability = 1;
+
+function getStar() {
+    let star_chars = ['*', '.', '+', '·', '°'];
+    return star_chars[Math.floor(Math.random() * star_chars.length)]
+}
+
+/** Возращает true или false в зависимости от starProbability */
+function doStar(coef = 1) {
+    return Math.floor(Math.random() * coef * (100 / starProbability)) == 0;
+}
+
+function initBuffer(){
+    const buffer = [];
+    for (let i = 0; i < resolution.heigth; i++) {
+        buffer.push([]);
+        for (let j = 0; j < resolution.width; j++) {
+            if (doStar()){
+                buffer[i].push(getStar())
+            }
+            else {
+                buffer[i].push(spaceChar)
+            }
+        }
+        buffer[i].push('<br/>')
+    }
+
+    return buffer;
+}
+
+function updateBuffer(buffer, textLength) {
+    const textBound = Math.floor((resolution.width - textLength) / 2);
+    console.log('textBound :>> ', textBound);
+    console.log('textLength :>> ', textLength);
+    for (let i = 0; i < resolution.heigth; i++) {
+        for (let j = 0; j < resolution.width; j++) {
+            if (doStar(100)){
+                buffer[i][j] = getStar()
+            } else if (doStar()) {
+                buffer[i][j] = spaceChar;
+            }
+
+            if (i == Math.floor(resolution.heigth / 2) && j >= textBound && j - textBound < textLength) {
+                const phrazeIndex = j-textBound;
+                if (phrazeIndex == 0) {
+                    buffer[i][j] = `<span id="phrase" style="color: gray;white-space: nowrap;">`;
+                } else if (phrazeIndex == textLength - 1) {
+                    buffer[i][j] = `</span>`;
+                } else {
+                    buffer[i][j] = '';
+                }
+                
+            }
+        }
+        buffer[i][resolution.width] = '<br/>'
+    }
+}
+
+/**
+ * 
+ * @param {string[][]} buffer 
+ */
+function updateContent(buffer) {
+    /** @type HTMLDivElement */
+    const el = document.querySelector('#content');
+
+    const text = buffer.map((e) => e.join('')).join('');
+
+    el.innerHTML = text;
+}
+
+function formatDistro(distro) {
+    return `${distro.description}${spaceChar}<span style="color:${distro.color}">${distro.name}</span>`;
+}
+
+function run() {
+    const buffer = initBuffer();
+
+    updateContent(buffer);
+
+    const phrases = [
+        "The stars are thinking...",
+        "A decision is forming...",
+        "The next distro will be decided soon...",
+        "Patience is a virtue in the cosmic dance...",
+        "The universe is aligning..."
+    ];
+
+    let text = phrases[0];
+    let textLength = text.length;
+
+
+    let phraseIndex = 1;
+
+    const interval = setInterval(()=> {
+        if (phraseIndex < phrases.length) {
+            text = phrases[phraseIndex];
+            textLength = text.length;
+            phraseIndex++;
+        } else {
+            const distro = distros[Math.floor(Math.random()*distros.length)];
+
+            text = formatDistro(distro);
+            textLength = distro.description.length + distro.name.length + 10;
+            clearInterval(interval);
+        }
+    }, 5000);
+
+    requestAnimationFrame(function animate() {
+        updateBuffer(buffer, textLength);
+        updateContent(buffer);
+        document.querySelector('#phrase').innerHTML=text;
+        requestAnimationFrame(animate);
+    })
+}
+
+run();
